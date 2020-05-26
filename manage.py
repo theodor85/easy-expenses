88cbe4289f9c060c23ff9b2e62d47facb479b6fb
Fedor Kosolapov <fedor_coder@mail.ru>
2020-05-26 16:29:30 +0700

    add vue blank app

--- manage.py
+++ manage.py
@@ -28,9 +28,10 @@ if __name__ == "__main__":
     current_path = Path(__file__).parent.resolve()
     sys.path.append(str(current_path / "easy_expenses"))
 
+    from django.conf import settings
     if settings.DEBUG:
         if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
             import ptvsd
             ptvsd.enable_attach(address=("0.0.0.0", 3000))
-    
+
     execute_from_command_line(sys.argv)
